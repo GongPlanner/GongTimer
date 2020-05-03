74eb6c2e46f36b670e3e1386a1f8253d88024e1f
yg1110 <younggil9488@gmail.com>
2020-05-04 01:44:49 +0900

    프로젝트 생성

---
+++ src/views/GongTimer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Clock />
+  </div>
+</template>
+
+<script>
+import Clock from "@/components/Clock.vue";
+
+export default {
+  name: "GongTimer",
+  components: {
+    Clock
+  }
+};
+</script>
---
+++ tests/unit/example.spec.js
@@ -0,0 +1,6 @@
+import { shallowMount } from "@vue/test-utils";
+import Clock from "@/components/Clock.vue";
+
+describe("Clock.vue", () => {
+  it("언젠간 할 수 있겠죠 ..?");
+});
