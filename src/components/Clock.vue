30da1f7850f9e6e3838f7334922697c5475a07b5
yg1110 <younggil9488@gmail.com>
2020-05-07 05:50:44 +0900

    시계 색깔 채우기

--- src/components/Clock.vue
+++ src/components/Clock.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="clock">
     <div class="inner">
+      <div class="mask full">
+        <div class="fill"></div>
+      </div>
+      <div class="mask half">
+        <div class="fill"></div>
+      </div>
       <div class="hour hand"></div>
       <div class="minute hand"></div>
       <div class="second hand"></div>
       <div class="graduations">
         <div class="graduation" v-for="i in 60" :key="i">
-          <!-- <p v-if="i % 5 === 0">{{ i }}</p> -->
+          <p v-if="i % 5 === 0">{{ i }}</p>
         </div>
       </div>
     </div>
+    <input type="range" v-model="timerRange" min="0" max="365" />
+    {{ timerRange }}
   </div>
 </template>
 
@@ -17,6 +25,17 @@
 import clock from "@/utils/Clock.js";
 export default {
   name: "Clock",
+
+  watch: {
+    timerRange: v => {
+      console.log(document.querySelector(".inner").style.width);
+      console.log(v);
+    }
+  },
+  data: () => ({
+    timerRange: 120
+  }),
+
   mounted() {
     new clock();
   },
