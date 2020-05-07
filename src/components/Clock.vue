c08833115b34bdd7e421333078367449069b8565
SsangWoo <tkddn204@gmail.com>
2020-05-07 11:34:56 +0900

    Clock 수정

--- src/components/Clock.vue
+++ src/components/Clock.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="clock">
+  <div class="clock" :style="timerCss">
     <div class="inner">
       <div class="mask full">
         <div class="fill"></div>
@@ -16,28 +16,52 @@
         </div>
       </div>
     </div>
-    <input type="range" v-model="timerRange" min="0" max="365" />
+    <input
+      name="timer-range"
+      type="range"
+      v-model="timerRange"
+      min="0"
+      max="360"
+    />
     {{ timerRange }}
   </div>
 </template>
 
 <script>
 import clock from "@/utils/Clock.js";
-import styles from "@/assets/css/Clock.sass";
 
 export default {
   name: "Clock",
 
-  watch: {
-    timerRange: v => {
-      // sass export한 변수 불러는 와지지만 css가 실시간 적용이 안됨.
-      if (v > 180) {
-        styles.degree1 = 180;
-        styles.degree2 = v - 180;
+  // watch: {
+  //   timerRange: v => {
+  //     // sass export한 변수 불러는 와지지만 css가 실시간 적용이 안됨.
+  //     // if (v > 180) {
+  //     //   styles.degree1 = 180;
+  //     //   styles.degree2 = v - 180;
+  //     // } else {
+  //     // }
+  //     // this.timerRange = v;
+  //     console.log(v);
+  //     // document.style.setProperty("--degree1", `${v}px`);
+  //   }
+  // },
+  computed: {
+    timerCss() {
+      let degree1 = 0;
+      let degree2 = 0;
+      if (this.timerRange > 180) {
+        degree1 = 180;
+        degree2 = this.timerRange - 180;
       } else {
-        styles.degree1 = v;
+        degree1 = this.timerRange;
       }
-      console.log(styles.degree1);
+      console.log(degree1, degree2);
+
+      return {
+        "--degree1": `${degree1}deg`,
+        "--degree2": `${degree2}deg`
+      };
     }
   },
   data: () => ({
